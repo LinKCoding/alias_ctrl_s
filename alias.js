3b945d004451fe626091380dae72c74eec82e310
linkcoding <kenny.lin.91@gmail.com>
2020-02-24 00:19:28 -0500

    removed comment

--- alias.js
+++ alias.js
@@ -2,7 +2,7 @@ window.onload = function () {
   document.addEventListener("keydown", function (e) {
     if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.key === "s") {
       e.preventDefault();
-      // Process the event here (such as click on submit button)
+      
       const buttons = document.getElementsByTagName('button')
       const saveButton = [...buttons].find(el => el.innerHTML === "Save")
       try {
