8be2b4cb9482419bd070a83af57ad4f692821f77
linkcoding <kenny.lin.91@gmail.com>
2020-02-23 23:56:09 -0500

    stoped cmd s default behavior

--- alias.js
+++ alias.js
@@ -1,3 +1,10 @@
 window.onload = function () {
-  console.log("page load!");
+  document.addEventListener("keydown", function (e) {
+    if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.key === "s") {
+      e.preventDefault();
+      // Process the event here (such as click on submit button)
+      console.log('ah ha!')
+    }
+  }, false);
 }
+
